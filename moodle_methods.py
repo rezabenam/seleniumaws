e58444403cd6ef0ec32698c64cd4df38fb093406
rezabenam <benam_reza@yahoo.com>
2022-03-02 11:32:06 -0800

    options added to the moodle_methods

--- moodle_methods.py
+++ moodle_methods.py
@@ -1,3 +1,20 @@
+
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.add_argument("--headless")
+options.add_argument("window-size=1400,1500")
+options.add_argument("--disable-gpu")
+options.add_argument("--no-sandbox")
+options.add_argument("start-maximized")
+options.add_argument("enable-automation")
+options.add_argument("--disable-infobars")
+options.add_argument("--disable-dev-shm-usage")
+
+driver = webdriver.Chrome(options=options)
+
+
+
 import sys
 from selenium import webdriver # import selenium to the file
 import moodle_locators as locators
@@ -8,8 +25,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select # <-- add this import for drop down lists
 from selenium.webdriver.common.keys import Keys
 
-s = Service(executable_path='/Users/reza/PycharmProjects/pythonProject/chromedriver')
-driver = webdriver.Chrome(service=s)
+#s = Service(executable_path='/Users/reza/PycharmProjects/pythonProject/chromedriver')
+#driver = webdriver.Chrome(service=s)
 
 # ----------------------------------------------------------
 def setUp():
